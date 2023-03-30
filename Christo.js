3de2b47e041f691ca09d7cb06cdeb481ff955777
Christo Shaji <Christo.Shaji@ust.com>
2023-03-30 14:48:26 +0530

    first commit

---
+++ Christo.js
@@ -0,0 +1,3 @@
+var firstName = "Dylan"; // inferred to type string
+// attempts to re-assign the value to a different type
+console.log(firstName);
---
+++ Christo.ts
@@ -0,0 +1,4 @@
+let firstName = "Dylan"; // inferred to type string
+// attempts to re-assign the value to a different type
+
+console.log(firstName);
